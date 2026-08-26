5ed908594e868e5ac2f969822f6763938bf2db84
azan-personal <112457939+azanra@users.noreply.github.com>
2026-08-26 08:20:33 +0700

    refactor: consume data with hook on font dropdown

--- src/pages/components/FontDropdown.tsx
+++ src/pages/components/FontDropdown.tsx
@@ -1,44 +1,42 @@
-import {
-  Dropdown,
-  DropdownMenuItem,
-  DropdownMenuItems,
-  DropdownTrigger,
-} from "./Dropdown";
-
 import ArrowDown from "../../assets/svg/ArrowDown.svg?react";
+import Dropdown from "../../shared/components/Dropdown";
+import { useFont } from "../../shared/hooks/useFont";
+import { FONT_OPTIONS } from "../constants/fontConst";
+
 import dictionaryUtils from "../utils/dictionaryUtils";
 
-const FontDropdown = ({ setCurrentFont, currentFont }) => {
-  const fonts = ["Sans Serif", "Serif", "Monospace"];
+const FontDropdown = () => {
+  const { currentFont, setCurrentFont } = useFont();
+
   return (
     <div className="flex flex-1 justify-end">
       <Dropdown>
-        <DropdownTrigger>
+        <Dropdown.Trigger>
           <div className="flex items-center border-r-1">
             <p className="font-bold">{currentFont}</p>
             <div className="mx-[16px]">
               <ArrowDown />
             </div>
           </div>
-        </DropdownTrigger>
-        <DropdownMenuItem>
-          {fonts.map((font) => {
+        </Dropdown.Trigger>
+        <Dropdown.MenuItem>
+          {FONT_OPTIONS.map((font) => {
             return (
-              <DropdownMenuItems
+              <Dropdown.MenuItems
                 key={font}
                 onClick={() => setCurrentFont(font)}
               >
                 <p
                   className={`${dictionaryUtils.getCurrentFont(
-                    font
+                    font,
                   )} font-bold py-[8px] hover:text-(--purple)`}
                 >
                   {font}
                 </p>
-              </DropdownMenuItems>
+              </Dropdown.MenuItems>
             );
           })}
-        </DropdownMenuItem>
+        </Dropdown.MenuItem>
       </Dropdown>
     </div>
   );
