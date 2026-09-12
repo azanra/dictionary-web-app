21d24eb59a3f5576367031cc9f967295b6abb615
azan-personal <112457939+azanra@users.noreply.github.com>
2026-09-12 14:09:41 +0700

    refactor: use proper header semantic in error page

--- src/pages/components/Error.tsx
+++ src/pages/components/Error.tsx
@@ -2,10 +2,10 @@ const Error = () => {
   return (
     <div className="px-[24px] py-[128px] flex flex-col gap-[20px]">
       <div className="flex flex-col gap-[48px]">
-        <h1 className="text-[64px] text-center">😕</h1>
-        <h2 className="text-preset-3 text-center font-bold">
+        <span className="text-[64px] text-center">😕</span>
+        <h1 className="text-preset-3 text-center font-bold">
           No Definitions Found
-        </h2>
+        </h1>
       </div>
       <p className="text-preset-4 text-center text-(--neutral-500)">
         Sorry pal, we couldn't find Definitions for the word you were looking
